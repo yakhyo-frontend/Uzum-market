aef206e6a1c3be092881f1024ead30b45c245e54
Yaxa <yexzer11@example.com>
2026-06-13 18:59:09 +0500

    uzum-market has done

--- main.js
+++ main.js
@@ -29,55 +29,106 @@ setInterval(nextSlide, 3000);
 
 // Cards
 
-const API = `https://fakestoreapi.com/products`;
-const cardsDiv = document.querySelector(".cards");
+const cardsDiv1 = document.querySelector(".cards-1");
+const cardsDiv2 = document.querySelector(".cards-2");
+const cardsDiv3 = document.querySelector(".cards-3");
 
-const getProducts = (url) => {
-  fetch(url, {
-    method: "GET",
-  })
-    .then((response) => response.json())
+const getProducts1 = () => {
+  fetch(`https://fakestoreapi.com/products/?limit=8`)
+    .then((res) => res.json())
     .then((data) => {
-      showProducts(data);
+      showProducts1(data);
     })
-    .catch((error) => {
-      console.error(error);
-      Toastify({
-        text: "Invalid API URL",
-        duration: 3000,
-        close: true,
-        gravity: "top",
-        position: "right",
-        style: {
-          background: "linear-gradient(to right, #b00000, #c93d3d)",
-        },
-      }).showToast();
-    });
+    .catch((err) => console.error(err));
 };
 
-getProducts(API);
-
-function showProducts(data) {
-  cardsDiv.innerHTML = "";
-
+function showProducts1(data) {
+  cardsDiv1.innerHTML = "";
   data.forEach((element) => {
-    const { id, image, price, description, rating } = element;
-
-    cardsDiv.innerHTML += `
-            <div class="card">
-              <div class="wrapper-card">
-              <p class="heart-card">♡</p>
-              <img class="card-img" src="${image}" alt="" />
-              
-              </div>
-
-              <p class="description">${description.slice(0, 80)}</p>
-              <p class="rating">☆ ${rating.rate}</p>
-              <div class="price-box">
-              <h3 class="price">${price} so'm</h3>
-              <h3 class="add">+</h3>
-              </div>
-            </div>
+    const { image, price, description, rating } = element;
+    cardsDiv1.innerHTML += `
+        <div class="card">
+          <div class="wrapper-card">
+            <p class="heart-card">♡</p>
+            <img class="card-img" src="${image}" />
+          </div>
+          <p class="description">${description.slice(0, 60)}...</p>
+          <p class="rating">☆ ${rating.rate}</p>
+          <div class="price-box">
+            <h3 class="price">${price} so'm</h3>
+            <h3 class="add">+</h3>
+          </div>
+        </div>
     `;
   });
 }
+
+const getProducts2 = () => {
+  fetch(`https://fakestoreapi.com/products/?limit=16`)
+    .then((res) => res.json())
+    .then((data) => {
+      showProducts2(data);
+    })
+    .catch((err) => console.error(err));
+};
+
+function showProducts2(data) {
+  cardsDiv2.innerHTML = "";
+
+  data.forEach((element, index) => {
+    if (index < 8) return;
+
+    const { image, price, description, rating } = element;
+    cardsDiv2.innerHTML += `
+        <div class="card">
+          <div class="wrapper-card">
+            <p class="heart-card">♡</p>
+            <img class="card-img" src="${image}" />
+          </div>
+          <p class="description">${description.slice(0, 60)}...</p>
+          <p class="rating">☆ ${rating.rate}</p>
+          <div class="price-box">
+            <h3 class="price">${price} so'm</h3>
+            <h3 class="add">+</h3>
+          </div>
+        </div>
+    `;
+  });
+}
+
+const getProducts3 = () => {
+  fetch(`https://fakestoreapi.com/products/?limit=20`)
+    .then((res) => res.json())
+    .then((data) => {
+      showProducts3(data);
+    })
+    .catch((err) => console.error(err));
+};
+
+function showProducts3(data) {
+  cardsDiv3.innerHTML = "";
+
+  data.forEach((element, index) => {
+    if (index < 16) return;
+
+    const { image, price, description, rating } = element;
+    cardsDiv3.innerHTML += `
+        <div class="card">
+          <div class="wrapper-card">
+            <p class="heart-card">♡</p>
+            <img class="card-img" src="${image}" />
+          </div>
+          <p class="description">${description.slice(0, 60)}...</p>
+          <p class="rating">☆ ${rating.rate}</p>
+          <div class="price-box">
+            <h3 class="price">${price} so'm</h3>
+            <h3 class="add">+</h3>
+          </div>
+        </div>
+    `;
+  });
+}
+
+getProducts1();
+getProducts2();
+getProducts3();
